7f12774ac4fb87f39977025db4b896cfb0da7b54
Pavel <pavel.holod2001@gmail.com>
2022-11-21 03:57:59 +0300

    reactTS

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {createElement as e, useState} from 'react';
+import {Product} from './components/product'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto max-w-2xl pt-5">
+      <Product />
+      <Product />
+      <Product />
+      <Product />
+      <Product />
     </div>
-  );
+  )
 }
 
 export default App;
